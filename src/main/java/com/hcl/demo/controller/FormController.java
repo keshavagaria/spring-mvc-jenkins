fc44ba7c7f92c31e998e591bd0dd3973959e7f1e
Vishal Singh <vishal267s@gmail.com>
2025-02-22 23:50:19 +0530

    first push

---
+++ src/main/java/com/hcl/demo/controller/FormController.java
@@ -0,0 +1,32 @@
+package com.hcl.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.hcl.demo.entity.Student;
+
+@Controller
+public class FormController {
+
+		@RequestMapping("/home")
+		public String home() {
+			
+			return "complex-form";
+		}
+	
+		
+		@RequestMapping(value = "/processform",method = RequestMethod.POST)
+		public String handleForm(@ModelAttribute("student") Student student,
+															BindingResult result) {    // to handle the errors
+			System.out.println(student);
+			if(result.hasErrors()) {
+				
+				return "complex-form";
+			}
+			return "success";
+		}
+
+}
